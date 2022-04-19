aeb53f42388e4f918b07a57105bc3de24b943dee
kaskiv <kaskiv11gmail.com>
2022-04-19 13:22:32 +0300

    Add project files.

---
+++ Lab4.2/Lab4.2.cpp
@@ -0,0 +1,35 @@
+﻿// Lab4.2.cpp 
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+string ProcessTXT(char* fname) {
+	ifstream fin(fname);
+	string s;
+	string w;
+	int k = 0;
+	while (fin >> s)
+	{
+		if (s[0] == 'a') {
+			w = s;
+		}
+	}
+	return w;
+}
+
+
+
+
+int main()
+{
+	char name[6] = "t.txt";
+	cout << "Last word start of 'a': " << ProcessTXT(name) << endl;
+
+	cin.get();
+	return 0;
+}
---
+++ UnitTest1Lab4.2/UnitTest1Lab4.2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab4.2/Lab4.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1Lab42
+{
+	TEST_CLASS(UnitTest1Lab42)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+			string str = "abstm";
+			string t = ProcessTXT("../t.txt");
+			Assert::AreEqual(str,  t);
+		}
+	};
+}
